8675324504f4016d12bee99fc12c273b7c877db0
dev <khfarhad6@gmail.com>
2024-10-27 03:02:34 +0330

    version 3.1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let count_display = document.querySelector(".countdisplay")
 let counter = 0
 
 function Ready(){
-    version.innerHTML = "version 3"
+    version.innerHTML = "version 3.1"
     count_display.innerHTML = (`count : ${counter}`)
 }
 
